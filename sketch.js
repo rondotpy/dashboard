0a75ca748791ea9d995106dd41b61cbec12aa063
Ronak <ronak.jm1991@gmail.com>
2019-03-29 00:08:17 -0600

    Change

--- sketch.js
+++ sketch.js
@@ -1,6 +1,12 @@
-function setup() {
+var branch;
 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    branch = loadJSON("Branch.json")
 }
 function draw() {
-
+    background(0)
+    fill(255)
+    text(branch.swtMER01, 10, 50)
+    console.log(branch.swtMER01)
 }
