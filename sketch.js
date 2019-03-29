a8174f3d84a09dc6eaaca434617b82bbf889ee62
Ronak <ronak.jm1991@gmail.com>
2019-03-29 01:23:00 -0600

    Change

--- sketch.js
+++ sketch.js
@@ -1,12 +1,25 @@
-var branch;
-
+let counterN = 0;
+let result; 
+function preload() {
+    result = loadStrings('switch.txt');
+  }
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    branch = loadJSON("Branch.json")
 }
 function draw() {
-    background(0)
-    fill(255)
-    text(branch.swtMER01, 10, 50)
-    console.log(branch.swtMER01)
+    background(0);
+    //console.log(counterN)
+    if(counterN > 3000){
+        result = loadStrings('switch.txt');
+        counterN = 0
+    }
+    let j = 32;
+    for(let i = 0; i < result.length; i++){
+        fill(0, 200, 150);
+        //console.log(result[i])
+        textSize(32);
+        text(result[i], 10, j);
+        j = j + 32;
+    }
+    counterN++;
 }
